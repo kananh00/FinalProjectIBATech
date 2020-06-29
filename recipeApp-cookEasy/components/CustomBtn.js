1a2919f9a01002175d9ffb8e50f7071be2ceacf2
kenan00 <kenan.huseynli44@gmail.com>
2020-06-29 22:14:11 +0400

    old CustomBtn

--- recipeApp-cookEasy/components/CustomBtn.js
+++ recipeApp-cookEasy/components/CustomBtn.js
@@ -1,30 +1,11 @@
-import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
-import { CustomText } from "./CustomText";
-import { COLORS } from "../styles/color";
 
-export const CustomBtn = ({ title, onPress, width, style, titleStyle = {} }) => (
-  <TouchableOpacity onPress={onPress} style={{ width }}>
-    <View style={[styles.btn, style]}>
-      <CustomText weight="bold" style={{ ...styles.title, ...titleStyle }}>
-        {title}
-      </CustomText>
-    </View>
-  </TouchableOpacity>
-);
-
-const styles = StyleSheet.create({
-  btn: {
-    height: 55,
-    backgroundColor: COLORS.CREATE_ACCOUNT_COLOR,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 18,
-  },
-  title: {
-    fontSize: 18,
-    color: COLORS.BUTTON_TEXT,
-    textTransform: "uppercase",
-  },
-});
+export const CustomBtn = () => {
+    return (
+        <View>
+            <Text>CustomBtn</Text>
+        </View>
+    )
+}
