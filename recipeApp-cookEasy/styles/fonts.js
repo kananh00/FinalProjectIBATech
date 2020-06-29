298b42b4188fd0ab4d804809c824fa85d3e899d7
CemileIsrafilzade <israfilzadecemile@gmail.com>
2020-06-29 19:23:49 +0400

    added fonts to assets

--- recipeApp-cookEasy/styles/fonts.js
+++ recipeApp-cookEasy/styles/fonts.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import {View,Text} from  'react-native'
 
-export const Fonts = () =>{
-    return(
-        <View>
-            <Text>fontssssssssssssssss </Text>
-        </View>
-    )
-}
+import MontserratRegular from "../assets/fonts/Montserrat-Regular.ttf";
+import MontserratMedium from "../assets/fonts/Montserrat-Medium.ttf";
+import MontserratSemiBold from "../assets/fonts/Montserrat-SemiBold.ttf";
+import MontserratBold from "../assets/fonts/Montserrat-Bold.ttf";
+
+export const loadFonts = () => {
+  return Font.loadAsync({
+    MontserratRegular,
+    MontserratMedium,
+    MontserratSemiBold,
+    MontserratBold,
+  });
+};
+
+export const FONT_FAMILIES = Object.freeze({
+  regular: "MontserratRegular",
+  medium: "MontserratMedium",
+  semi: "MontserratSemiBold",
+  bold: "MontserratBold",
+});
