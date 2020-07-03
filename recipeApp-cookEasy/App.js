43850bf9eb42ac50f06ddea6d5ba566926c07a61
CemileIsrafilzade <israfilzadecemile@gmail.com>
2020-07-03 19:46:41 +0400

    update-app.js

--- recipeApp-cookEasy/App.js
+++ recipeApp-cookEasy/App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
 import { AppLoading } from "expo";
+import { Provider } from "react-redux";
+
+import { RootDrawer } from "./navigation/RootDrawer";
 import { loadFonts } from "./styles/fonts";
-import { HomeScreen } from './screens';
-
-
+import store from "./store";
 
 export default function App() {
-    const [loaded, setLoaded] = useState(false);
-    if (!loaded) {
-      return (
-        <AppLoading startAsync={loadFonts} onFinish={() => setLoaded(true)} />
-      );
-     
-    }
-     return  <HomeScreen/>
+  const [loaded, setLoaded] = useState(false);
+  if (!loaded) {
+    return (
+      <AppLoading startAsync={loadFonts} onFinish={() => setLoaded(true)} />
+    );
   }
+  return (
+    <Provider store={store}>
+      <RootDrawer />
+    </Provider>
+  );
+}
