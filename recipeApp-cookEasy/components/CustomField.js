ab43c4fce9ab86a5347ea9160a0b54300ee57291
CemileIsrafilzade <israfilzadecemile@gmail.com>
2020-07-03 19:12:38 +0400

    customfield

---
+++ recipeApp-cookEasy/components/CustomField.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {TextInput,StyleSheet,View} from 'react-native'
+import { COLORS } from '../styles/color'
+import { CustomText } from "./CustomText";
+import { FONT_FAMILIES } from "../styles/fonts";
+export const CustomField = ({
+    title,
+    width,
+    style,
+    ...rest
+  }) => (
+    <View>
+      <CustomText weight="semi" style={styles.title}>
+        {title}
+      </CustomText>
+      <TextInput {...rest} placeholder = {title} style={[styles.field, style]} />
+    </View>
+  );
+  
+
+
+const styles = StyleSheet.create({
+    title: {
+      opacity: 0.75,
+      textAlign: "center",
+      fontSize: 12,
+      
+    },
+    field: {
+      height: 42,
+    //   textAlign: "center",
+      paddingHorizontal: 15,
+      fontSize: 14,
+      backgroundColor: COLORS.FIELD_BG,
+      borderRadius: 10,
+      marginTop: 7,
+      marginBottom: 10,
+      fontFamily: FONT_FAMILIES.bold,
+      minWidth: "90%"
+    },
+  });
+  
