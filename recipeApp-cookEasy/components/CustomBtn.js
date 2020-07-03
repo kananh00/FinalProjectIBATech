a4143c1aceeb0164e6bc9739beecef87167a2025
CemileIsrafilzade <israfilzadecemile@gmail.com>
2020-07-03 18:17:07 +0400

    add style

--- recipeApp-cookEasy/components/CustomBtn.js
+++ recipeApp-cookEasy/components/CustomBtn.js
@@ -21,6 +21,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 18,
+    minWidth:260,
+    marginVertical:10,
+
   },
   title: {
     fontSize: 18,
