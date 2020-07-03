77afe4fccf8d0e1354d8d096ee0e6980756e52c3
CemileIsrafilzade <israfilzadecemile@gmail.com>
2020-07-03 19:37:05 +0400

    update-index

--- recipeApp-cookEasy/screens/HomeScreen.js
+++ recipeApp-cookEasy/screens/HomeScreen.js
@@ -1,10 +1,73 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {
+    View,Text,Image,StyleSheet,TouchableOpacity
+} from 'react-native'
+import { IMAGES } from '../styles/images'
+import { COLORS } from '../styles/color'
+import { FONT_FAMILIES, loadFonts } from '../styles/fonts'
+import { CustomText } from '../components/CustomText'
+import { CustomBtn } from '../components/CustomBtn'
+import { useNavigation } from "@react-navigation/native";
 
-export const HomeScreen = ()=>{
-    return(
-        <View>
-            <Text>helloooooo</Text>
-        </View>
-    )
-}
+
+ export const HomeScreen = ({navigation}) =>{
+     return(
+         <View style={styles.container}>
+            <View style={styles.upper}>
+                <CustomText weight = "bold" style={styles.cookeasytxt}>cookEasy</CustomText>
+                <Image  style={styles.welcomeimg} source={IMAGES.welcomeImg}/>
+            </View>
+            <View style={styles.below}>
+                    <CustomText weight = "medium" style={styles.slogan}>For learning new recipes, Let’s start </CustomText>
+                    <CustomBtn title='create account'
+                        onPress={() => navigation.navigate('SIGNUPPAGE')}
+                    />
+                <View style={{flexDirection:'row'}}>
+                    <CustomText>already have, </CustomText>
+                    <TouchableOpacity onPress={() => navigation.navigate('LOGINPAGE')}>
+                        <CustomText weight = "semi" style={styles.logintxt} >log in</CustomText>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        
+         </View>
+     )
+ } 
+ const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+    upper:{
+    justifyContent:'center',
+    // paddingTop:15,
+    backgroundColor:COLORS.PRIMARY,
+    borderBottomLeftRadius:200,
+    borderBottomRightRadius:200,
+    alignItems:'center',
+    },
+    welcomeimg:{
+        width:300,
+        height:300,
+    },
+    cookeasytxt:{
+        // fontFamily:FONT_FAMILIES.bold,
+        fontSize:48,
+        color:'white',
+        marginTop: 40,
+
+    },
+    slogan:{
+        fontSize:30,
+            // fontFamily:FONT_FAMILIES.regular,
+        textAlign:'center',
+        marginVertical:15
+    },
+    below:{
+        alignItems:'center',
+        justifyContent:'center',
+        paddingHorizontal:25
+    },
+    logintxt:{
+        fontWeight:"bold"
+    }
+ })
