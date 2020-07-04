f6434a591764877ec72273bbca58dfcfd9b4e1f1
kenan00 <kenan.huseynli44@gmail.com>
2020-07-04 16:35:07 +0400

    add ListScreenStyles

--- recipeApp-cookEasy/screens/ListScreen/RecipesItem.js
+++ recipeApp-cookEasy/screens/ListScreen/RecipesItem.js
@@ -3,6 +3,7 @@ import { StyleSheet,Text, View, TouchableOpacity, Image } from "react-native";
 
 import { COLORS } from "../../styles/color";
 import { CustomText } from "../../components/CustomText";
+import {GLOBAL_STYLES} from '../../styles/globalStyles';
 export const RecipesItem = ({
   title,
   count,
@@ -12,11 +13,14 @@ export const RecipesItem = ({
     <TouchableOpacity
       style={styles.container}
     >
-      <View>
-        <CustomText style={styles.name}>{title}</CustomText>
+      <View style = {styles.row}>
+        <CustomText weight = "semi" style={styles.name}>{title}</CustomText>
+        <View style = {styles.countWrapper}>
         <CustomText style={styles.count}>
           {count} {unit}
+        
         </CustomText>
+        </View>
 
       </View>
     </TouchableOpacity>
@@ -24,4 +28,30 @@ export const RecipesItem = ({
 };
 
 const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // alignItems: "center",
+    marginLeft: 25,
+    marginRight: 25,
+    borderWidth: 2,
+    padding: 20,
+    borderRadius: GLOBAL_STYLES.BORDER_RADIUS,
+    borderColor: "transparent",
+    backgroundColor: COLORS.PRIMARY,
+    marginBottom: 10,
+    paddingVertical: 7,
+    alignItems: "center",
+  },
+  countWrapper: {
+    borderWidth: 2,
+    backgroundColor: "white",
+    borderRadius: GLOBAL_STYLES.BORDER_RADIUS,
+    width: 100,
+    paddingVertical: 5,
+    alignItems: "center",
+  },
+  count: {
+    
+  }
 })
--- recipeApp-cookEasy/screens/ListScreen/index.js
+++ recipeApp-cookEasy/screens/ListScreen/index.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import {Image, StyleSheet, ScrollView, Text, View } from "react-native";
+import {Image, StyleSheet, ScrollView, Text, View, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { connect } from "react-redux";
 
 import { RecipeContent } from "./RecipeContent";
 import { MaterialIcons } from '@expo/vector-icons';
-
+import { ICONS } from '../../styles/icon'
+import {GLOBAL_STYLES} from '../../styles/globalStyles';
 import {
   selectSingleRecipeByID,
 
 } from "../../store/data";
 import { CustomText } from "../../components/CustomText";
+import { COLORS } from "../../styles/color";
 
 const mapStateToProps = (state, { route }) => ({
   recipe: selectSingleRecipeByID(
@@ -33,24 +35,57 @@ export const ListScreen = connect(mapStateToProps)(
    
     return (
       <ScrollView>
+        <View style = {styles.recipeText}>
       <View style = {styles.imgWrapper}>
         <Image style={styles.recipeImg} source={{ uri: image }} />
         <MaterialIcons style = {styles.backArrow} onPress = {navigations.goBack} name="arrow-back" size={35} color="white" />
         
       </View>
-      <View style = {styles.contentWrapper}>
-        <CustomText weight = "semi" style = {styles.title}>{title}</CustomText> 
-        <View style = {styles.cover}>
-        <View style = {styles.row}>
-          <CustomText>{duration}</CustomText>
-          <CustomText>{portion} person</CustomText> 
-          <CustomText>add to wishlist</CustomText> 
-        </View> 
+      <View style = {styles.content}>
+        <View style = {styles.favorite}>
+          <Image  style={styles.icons}  source={ICONS.heartEmpty}/>
         </View>
-        <CustomText>{desc}</CustomText>  
+        <View style = {styles.contentWrapper}>
+          <CustomText weight = "semi" style = {styles.title}>{title}</CustomText> 
+          <View style = {styles.cover}>
+            <View style = {styles.row}>
+
+              <View style = {styles.iconWrapper}>
+                <Image  style={styles.icons}  source={ICONS.clock}/>
+                <CustomText>{duration}</CustomText>
+              </View>
+
+              <View style = {styles.iconWrapper}>
+                <Image  style={styles.icons}  source={ICONS.dinner}/>
+                <CustomText>{portion} person</CustomText> 
+              </View>
+
+              <TouchableOpacity>
+                <View style = {styles.iconWrapper}>
+                  <Image  style={styles.icons}  source={ICONS.eventColored}/>
+                  <CustomText>add to wishlist</CustomText> 
+                </View>
+              </TouchableOpacity>
+
+            </View>
+          
+          {/* <View style = {styles.row}>  
+            <CustomText>{duration}</CustomText>
+            <CustomText>{portion} person</CustomText> 
+           <TouchableOpacity>
+            <CustomText>add to wishlist</CustomText> 
+          </TouchableOpacity>
+          </View>  */}
+          </View>
+          <CustomText weight = "semi" style = {styles.text}>How to make it</CustomText> 
+          <CustomText  style = {styles.description}>{desc}</CustomText>  
+          <CustomText weight = "semi" style = {styles.text}>Ingredients</CustomText> 
+        </View>
+        
         <RecipeContent
-          ingredients={recipe.ingredients}
-        />
+            ingredients={recipe.ingredients}
+          />
+        </View>
       </View>
       </ScrollView>
     );
@@ -63,8 +98,20 @@ const styles = StyleSheet.create({
       title: {
           color: "black",
           fontSize: 30,
-          justifyContent: "center",
-          textAlign: "center",
+          marginBottom: GLOBAL_STYLES.MARGIN,
+      },
+      favorite: {
+        borderWidth: 4,
+        borderColor: "white",
+        backgroundColor: COLORS.PRIMARY,
+        borderRadius: 100,
+        width: 60,
+        height: 60,
+        alignItems: "center",
+        justifyContent: "center",
+        marginLeft: 50,
+        marginTop: -40,
+        marginBottom: 10,
       },
       imgWrapper: 
       {
@@ -75,13 +122,21 @@ const styles = StyleSheet.create({
     backArrow: {
         marginTop: 20,
     },
+    icons:{
+        width:30,
+        height:30,
+        marginBottom: 5,
+    },
     contentWrapper:{
-    marginTop: -30,
-    backgroundColor: "white",
-    borderTopStartRadius: 40,
-    borderTopEndRadius: 40,
-    paddingTop: 15,
     alignItems: "center",
+    },
+    content: {
+      flex: 1,
+      marginTop: -30,
+      backgroundColor: "white",
+      borderTopStartRadius: 40,
+      borderTopEndRadius: 40,
+      paddingTop: 15,
     },
     row: {
       flexDirection: "row",
@@ -90,5 +145,17 @@ const styles = StyleSheet.create({
     },
     cover: {
       width: "80%",
+    },
+    text: {
+      fontSize: 22,
+      margin: GLOBAL_STYLES.MARGIN,
+    },
+    description: {
+      paddingRight: 15,
+      paddingLeft: 15,
+    },
+    iconWrapper: {
+      flexDirection: "column",
+      alignItems: "center",
     }
 })
