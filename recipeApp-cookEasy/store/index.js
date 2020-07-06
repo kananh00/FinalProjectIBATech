351a8b05d81d2e241366496d65bc03612e13ff1f
CemileIsrafilzade <israfilzadecemile@gmail.com>
2020-07-06 11:08:01 +0400

    add store/index

--- recipeApp-cookEasy/store/index.js
+++ recipeApp-cookEasy/store/index.js
@@ -1,24 +1,49 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers,applyMiddleware } from "redux";
 import { dataReducer } from "./data";
 import { updateAS, getDataFromAS } from "../utils/storeDataAS";
-
+import {AsyncStorage} from 'react-native'
+import {persistStore,persistReducer} from 'redux-persist';
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from "redux-devtools-extension";
 import {
     MODULE_NAME as recipesModuleName,
     dataReducer as recipesReducer,
   } from "./data";
+
+  import {
+    MODULE_NAME as authModuleName,
+    reducer as authReducer,
+  } from "./auth";
   
+const config = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
 const rootReducer = combineReducers({
   data: dataReducer,
+  [authModuleName]: authReducer,
 });
 // const rootReducer = combineReducers({
 //     [recipesModuleName]: recipesReducer,
     
 //   })
+  // const rootReducer = combineReducers({
+  //   [authModuleName]: authReducer,
+    
+  // })
   
 
-const store = createStore(rootReducer);
+
+ const rootPersistReducer = persistReducer(config, rootReducer);
+
+ const store = createStore(
+   rootPersistReducer,
+   composeWithDevTools(applyMiddleware(thunk))
+ );
+ 
+ export const persistor = persistStore(store);
 
 store.subscribe(() => updateAS(store));
-getDataFromAS(store);
 
 export default store;
